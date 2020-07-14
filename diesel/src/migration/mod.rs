f5bc0a85518df74524a16f12f70b72cbd61b6d76
Bryan Henry <bryanhenry522@gmail.com>
2020-07-14 01:41:29 -0700

    Allow non-static lifetime on boxed Migration

--- diesel/src/migration/mod.rs
+++ diesel/src/migration/mod.rs
@@ -20,7 +20,7 @@ pub trait Migration {
     }
 }
 
-impl Migration for Box<dyn Migration> {
+impl<'a> Migration for Box<dyn Migration + 'a> {
     fn version(&self) -> &str {
         (&**self).version()
     }
